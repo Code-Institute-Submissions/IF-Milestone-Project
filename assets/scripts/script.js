ed0070690b50c04fbdf31dfa1c451cece5992f8a
PokeyMcFork <pokeymcfork@gmail.com>
2020-06-12 17:23:13 +0000

    Reworked card constructor, added deck generation.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,71 +16,109 @@ $(document).ready(function(){
     generateYoutubeModal();
 });
 
-//don't forget to re-organise all this once you're done, this is pretty messy dude.
-
 function initGame(){
-    gameArea.start();
+    gameArea.generate();
 }
 
 //Blackjack Game Code
+//Global Variables
+var deck = [];
+var playerHand = [];
+var dealerHand = [];
+var winLossRate = [0,0];
 
 //card class
 class card {
-    constructor(face, suit) {
-        //This hits a really weird taxonomic problem where I don't think there's a specific name for non-face cards,
-        //so I need to differentiate the numeric values of the cards for the sake of sorting in the handCalc function.
-        //For simplicity's sake, this.face refers to the proper name of the card,
-        //And this.value is the card's numeric value in a hand under normal conditions, with the exception of the ace, which needs special handling.
-        //This will also allow for a nice and easy string literal for pulling the card art from the image folder.
-        this.face = face;
-        this.suit = suit;
-        this.flipped = false;
-        //And that whole bit above leads to this switch statement which properly assigns the numeric values for sorting.
-        switch(this.face){
-            case "ace":
-                this.value = 0;//This is a little odd, but I need the ace last in the sort
+    constructor(number, suit) {
+        switch(number){
+            case 0:
+                this.rank = "ace";
+                this.value = 0;
                 break;
-            case "2"://Don't much like having the values assigned like this but it's too awkward otherwise, as it'd need a specific face value only certain cards have
+            case 1:
+                this.rank = "2";
                 this.value = 2;
                 break;
-            case "3"://while that'd assuredly have a smaller memory footprint, this is faster to code.
+            case 2:
+                this.rank = "3";
                 this.value = 3;
                 break;
-            case "4":
+            case 3:
+                this.rank = "4";
                 this.value = 4;
                 break;
-            case "5":
+            case 4:
+                this.rank = "5";
                 this.value = 5;
                 break;
-            case "6":
+            case 5:
+                this.rank = "6";
                 this.value = 6;
                 break;
-            case "7":
+            case 6:
+                this.rank = "7";
                 this.value = 7;
                 break;
-            case "8":
+            case 7:
+                this.rank = "8";
                 this.value = 8;
                 break;
-            case "9":
+            case 8:
+                this.rank = "9";
                 this.value = 9;
                 break;
-            case "10":
+            case 9:
+                this.rank = "10";
                 this.value = 10;
                 break;
-            case "jack":
+            case 10:
+                this.rank = "jack";
                 this.value = 10;
                 break;
-            case "queen":
+            case 11:
+                this.rank = "queen";
                 this.value = 10;
                 break;
-            case "king":
+            case 12:
+                this.rank = "king";
                 this.value = 10;
                 break;
             default:
-                console.log("this should never happen, default switch case in card constructor being triggered"); //debug error message, to be removed for deployment branch
+                console.log("Invalid Rank Number"); //debug error message, to be removed for deployment branch
+                break;
+        };
+        switch(suit){
+            case 0:
+                this.suit = "clubs";
+                break;
+            case 1:
+                this.suit = "diamonds";
+                break;
+            case 2:
+                this.suit = "hearts";
+                break;
+            case 3:
+                this.suit = "spades";
+                break;
+            default:
+                console.log("Invalid Suit Number");
                 break;
         };
     }
+}
+
+//Deck Generation Function
+function deckGeneration(){
+    deck.length = 0;//empty any potentially existing instance of the deck, to prevent it duplicating entries.
+    deck = [];
+    for(var suitNo = 0; suitNo < 4; suitNo++){ //condensed it all down to two for loops, and a two switch statements in a constructor.
+        for(var rankNo = 0; rankNo< 13; rankNo++){
+            deck.push(new card(rankNo, suitNo));
+        }
+    }
+    console.log(deck);
+    shuffle(deck);
+    console.log(deck);
 }
 
 //Implementation of a Fisher-Yates shuffle, taken from bost.ocks
@@ -103,7 +141,8 @@ function shuffle(array) {
   return array;
 };
 
-function handCalc(Hand){//Calculates the total value for the given hand.
+//Calculates the total value for the given hand.
+function handCalc(Hand){
     //Sorts the hand by in ascending order of card value, then reverses it for simpler calculation.
     var sortedHand = Hand.slice().sort(function(a,b){return a.value - b.value}).reverse();
 
@@ -128,14 +167,16 @@ function handCalc(Hand){//Calculates the total value for the given hand.
 var gameArea = {
     canvas : document.createElement("canvas"), //Creates a canvas object. 
     container: document.getElementsByClassName("GameContainer"),//Gets where the canvas is to be positioned.
-    start : function(){
+    generate : function(){
         //Width and height for the play-space.
         this.canvas.width = "700";
         this.canvas.height = "700";
         this.context = this.canvas.getContext("2d"); //Gets the context for image drawing and manipulation methods.
         this.container[0].insertBefore(this.canvas, null); //Adds the canvas to the DOM.
+
+        //then, need to generate the deck before any gamplay can occur.
+        deckGeneration();
     }
 }
 
 //Display Code
-
