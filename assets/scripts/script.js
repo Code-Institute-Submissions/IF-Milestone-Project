8ad84260b9971ea88cdf45b997e5a5bc525bc3e4
PokeyMcFork <pokeymcfork@gmail.com>
2020-06-19 11:51:45 +0000

    Added handling for getting a 21 during a draw.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,18 @@
 //Function to generate youtube vid in modal taken from indicated stackoverflow answer, modified slightly to not autoplay
-function generateYoutubeModal(){//actually, gonna see if i can optimise this function any after i'm done
+function generateYoutubeModal() {//actually, gonna see if i can optimise this function any after i'm done
     var trigger = $("#HowToPlay");
-    trigger.click(function(){//still need to get the trigger to do this, so can't avoid this bit
-        var targetModal = $(this).data("target"),
+    trigger.click(function() {//still need to get the trigger to do this, so can't avoid this bit
+        var targetModal = $(this).data("target");
         videoSource = $(this).attr("data-video");
         $(`${targetModal} iframe`).attr('src', videoSource);//fairly sure template literals are faster than just appending the string(only fairly sure, might have to research later)
-        $(`${targetModal} button.close`).click(function(){//fast and kinda dirty way of getting the elements in the modal, but as long as it works
+        $(`${targetModal} button.close`).click(function() {//fast and kinda dirty way of getting the elements in the modal, but as long as it works
             $(`${targetModal} iframe`).attr('src', ''); //destroys the videoplayer when the modal closes, should keep potential issues to a minimum.
         });
     });
 }
 
 //and then prime the modal when the page is done loading.
-$(document).ready(function(){
+$(document).ready(function() {
     generateYoutubeModal();
 });
 
@@ -208,9 +208,15 @@ var gameArea = {
         client.hit(deck);
         dealer.hit(deck);
         client.hit(deck);
-
+        
         loadImages(dealer.getHandImages(), drawImageCallback, dealer.originX, dealer.originY, dealer.cardWidth, dealer.cardHeight);
         loadImages(client.getHandImages(), drawImageCallback, client.originX, client.originY, client.cardWidth, client.cardHeight);
+
+        if(client.handCalc() == 21){
+            client.endTurn();
+            dealer.endTurn();
+            gameLogic(false);
+        }
     },
     resize: function(){
         if(client && dealer){
